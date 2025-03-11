9eefea99fc7d60e868949ab7f42b93d13f9fa7ce
Erkin Tunc <77968721+ErkinTunc@users.noreply.github.com>
2025-03-11 10:37:46 +0100

    little updates

--- Web/www/js/index.js
+++ Web/www/js/index.js
@@ -2,8 +2,8 @@ let heroContent = document.getElementById("hero-content");
 
 console.dir(window);
 
-function removeHeroContent(){
-    if (window.location.hash === "#side-drawer") {
+function removeHeroContent() {
+  if (window.location.hash === "#side-drawer") {
     // Perform actions when URL hash changes to #side-drawer
     console.log("URL has changed to #side-drawer");
     heroContent.classList.add("hero-content-display-none");
@@ -13,6 +13,5 @@ function removeHeroContent(){
     heroContent.classList.remove("hero-content-display-none");
   }
 }
-
 
 window.addEventListener("hashchange", removeHeroContent);
